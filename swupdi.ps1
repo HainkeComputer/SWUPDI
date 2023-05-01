d647a8d44ba773fef2b5967be76d1f1413bf6284
HainkeComputer <119492899+HainkeComputer@users.noreply.github.com>
2023-05-01 20:19:08 +0200

    Create swupdi.ps1

---
+++ swupdi.ps1
@@ -0,0 +1,21 @@
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$princ = New-Object System.Security.Principal.WindowsPrincipal($identity)
+if(!$princ.IsInRole( `
+[System.Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+$powershell = [System.Diagnostics.Process]::GetCurrentProcess()
+$psi = New-Object System.Diagnostics.ProcessStartInfo $powerShell.Path
+$script = $MyInvocation.MyCommand.Path
+$prm = $script
+foreach($a in $args) {
+$prm += ' ' + $a
+}
+$psi.Arguments = $prm
+$psi.Verb = "runas"
+[System.Diagnostics.Process]::Start($psi) | Out-Null
+return;
+}
+Write-Host "Ich habe Admin recht!"
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
